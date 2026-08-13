fa2b8cd5200830dad67135e6254df8875ff7dcea
Manuel Bärenz <mbaerenz@heilmannsoftware.de>
2026-08-13 09:53:41 +0200

    Simplification without type level uuid tagging

--- gravensteiner/src/Gravensteiner/Model.hs
+++ gravensteiner/src/Gravensteiner/Model.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 
-
+{- | Some types are tagged with a higher kinded datatype phase that can be used to mark parts of the data as being "not observed" via 'Maybe'.
+Sampling data on the other hand will always produce data, so can use 'Identity' as the phase.
+-}
 module Gravensteiner.Model where
 
 -- uuid
@@ -10,23 +14,29 @@ import Data.UUID (UUID)
 import Data.Text (Text)
 
 -- time
-
-import Data.Functor.Const (Const (..))
-import Data.Functor.Identity (Identity (..))
-import Data.Kind (Type)
-import Data.Map.Strict (Map)
-import Data.Map.Strict qualified as Map
 import Data.Time (Day, Year)
 
-newtype UUIDOf a = UUIDOf {unUUIDOf :: UUID}
-  deriving (Show, Eq, Ord)
+-- containers
+import Data.Map.Strict (Map)
+import Data.Map.Strict qualified as Map
 
-data NoUUIDOf a = NoUUID
-  deriving (Show, Eq)
+-- indexed-traversable
+import Data.Foldable.WithIndex (FoldableWithIndex)
+import Data.Functor.WithIndex (FunctorWithIndex)
+import Data.Traversable.WithIndex (TraversableWithIndex (..))
 
-data Person (uuidOf :: Type -> Type) = Person
+-- ghc
+import GHC.Records (HasField)
+
+-- | Recorded in serialisation to allow for future migrations
+version :: Int
+version = 1
+
+data Person = Person
   { name :: Text
-  , uuid :: uuidOf (Person uuidOf)
+  , email :: Maybe Text
+  , phone :: Maybe Text
+  , uuid :: UUID
   }
 
 -- | A number assumed to be between 0 and 1
@@ -40,44 +50,66 @@ data Colours = Colours
   }
   deriving (Show, Eq)
 
-data Fruit p (uuidOf :: Type -> Type) = Fruit
+data Fruit p = Fruit
   { colours :: p Colours
   , russet :: p Interval
-  , -- Further properties can be added here, such as size, shape, etc.
-    uuid :: uuidOf (Fruit p uuidOf)
+  , -- TODO Further properties to be added here, such as size, shape, etc.
+
+    observer :: p Person
+  {- ^ The person who observed the fruit and recorded its properties.
+  Often this will be the same as the pomologist who made the judgement, but not always.
+  -}
+  , -- TODO: photographs of the fruit
+    uuid :: UUID
   }
 
-data Collection (uuidOf :: Type -> Type) p = Collection
-  { fruits :: [Fruit p uuidOf]
+data Collection p = Collection
+  { fruits :: [Fruit p]
   , date :: p Day
-  , tree :: Tree p uuidOf
-  , uuid :: uuidOf (Collection p uuidOf)
+  -- ^ The date the collection was made
+  , tree :: Tree p
+  , uuid :: UUID
   }
 
-data Tree (uuidOf :: Type -> Type) p = Tree
+data Tree p = Tree
   { planted :: p Year
-  , uuid :: uuidOf (Tree p uuidOf)
+  , uuid :: UUID
   }
 
-class HasUUID a where
-  uuidLens :: (Functor f) => (uuidOf1 a -> f (uuidOf2 a)) -> a (uuidOf1 a) -> f (a (uuidOf2 a))
+data Cultivar = Cultivar
+  { name :: Text
+  , alternativeNames :: [Text]
+  , uuid :: UUID
+  }
 
-getUUID :: (HasUUID a) => a (uuidOf a) -> uuidOf a
-getUUID = getConst . uuidLens Const
+data Judgement p = Judgement
+  { pomologist :: Person
+  , tree :: UUID
+  , collection :: p UUID
+  -- ^ The collection that was used to make this judgement
+  , certainty :: Interval
+  -- ^ The probability the pomologist would bet on this judgement being correct.
+  , date :: p Day
+  }
 
-overUUID :: (HasUUID a) => (uuidOf1 a -> uuidOf2 a) -> a (uuidOf1 a) -> a (uuidOf2 a)
-overUUID f = runIdentity . uuidLens (Identity . f)
+newtype UUIDMap a = UUIDMap {getUUIDMap :: Map UUID a}
+  deriving newtype (Show, Eq, Functor, Foldable, FunctorWithIndex UUID, FoldableWithIndex UUID)
+  deriving stock (Traversable)
 
-setUUID :: (HasUUID a) => uuidOf2 a -> a (uuidOf1 a) -> a (uuidOf2 a)
-setUUID newUUID = overUUID (const newUUID)
+instance TraversableWithIndex UUID UUIDMap where
+  itraverse f (UUIDMap m) = UUIDMap <$> itraverse f m
 
-newtype UUIDMap (a :: (Type -> Type) -> Type) = UUIDMap {getUUIDMap :: Map UUID (a (NoUUIDOf a))}
+insert :: (HasField "uuid" a UUID) => a -> UUIDMap a -> UUIDMap a
+insert a (UUIDMap m) = UUIDMap $ Map.insert a.uuid a m
 
-lookup :: (HasUUID a) => UUIDOf a -> UUIDMap a -> Maybe (a (UUIDOf a))
-lookup uuid'@(UUIDOf uuid) (UUIDMap m) = fmap (setUUID uuid') (Map.lookup uuid m)
+data Observations = Observations
+  { collections :: UUIDMap (Collection Maybe)
+  , trees :: UUIDMap (Tree Maybe)
+  , judgements :: UUIDMap (Judgement Maybe)
+  }
 
 data Database = Database
   { people :: UUIDMap Person
-  , collections :: UUIDMap Collection
-  , trees :: UUIDMap Tree
+  , cultivars :: UUIDMap Cultivar
+  , observations :: Observations
   }
